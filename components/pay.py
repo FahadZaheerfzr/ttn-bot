ad0adb86c645ab66dfc78f06d06dbb90ce31fa73
FahadZaheerfzr <fahadzaheer19@gmail.com>
2023-03-24 16:45:46 +0500

    USDT added in place of BUSD

--- components/pay.py
+++ components/pay.py
@@ -9,13 +9,16 @@ from components import token_functions
 
 web3 = Web3(Web3.HTTPProvider(config.RPC_ADDRESS))
 
+
 def isMembershipMarkup(order_id, currency):
     markup = types.InlineKeyboardMarkup()
     markup.add(
-        types.InlineKeyboardButton(text="📲 Upgrade", callback_data=f"upgrade {order_id} {currency}"),
+        types.InlineKeyboardButton(
+            text="📲 Upgrade", callback_data=f"upgrade {order_id} {currency}"),
         types.InlineKeyboardButton(text="❌ Cancel", callback_data="cancel")
     )
     return markup
+
 
 def sameSection(message: types.CallbackQuery, bot: TeleBot, order_id, currency):
     order_info = DB['orders'].find_one({"_id": order_id})
@@ -25,32 +28,47 @@ def sameSection(message: types.CallbackQuery, bot: TeleBot, order_id, currency):
     prices = DB['panel'].find_one({"_id": 1})
 
     if not user_info:
-        bot.reply_to(message.message.chat.id, "You are not registered with bot yet, Press /start to register!")
+        bot.reply_to(message.message.chat.id,
+                     "You are not registered with bot yet, Press /start to register!")
 
     # if currency == "ttn":
     #     bot.answer_callback_query(message.id, "TTN Payments Are Currently Not Avilable", show_alert=True)
     #     return None
 
     bnb_price, ttn_price = prices['bnb_price'], prices['ttn_price']
-    newPrices = {"bnb": bnb_price, "ttn": ttn_price, "busd": prices['busd_price']}
+    newPrices = {"bnb": bnb_price, "ttn": ttn_price,
+                 "busd": prices['busd_price']}
     new_price_type = order_info['price'] / newPrices[currency]
 
-    payment_contract = web3.eth.contract(address=config.PAYMENT_CONTRACT, abi=config.PAYMENT_CONTRACT_ABI)
+    payment_contract = web3.eth.contract(
+        address=config.PAYMENT_CONTRACT, abi=config.PAYMENT_CONTRACT_ABI)
     balance = None
 
     if currency == "bnb":
         balance = web3.eth.get_balance(user_info['address']) / 10 ** 18
     elif currency == "ttn":
-        token = web3.eth.contract(address=web3.toChecksumAddress(config.TTN_CONTRACT), abi=config.TTN_CONTRACT_ABI)
-        balance = token.functions.balanceOf(user_info['address']).call() / 10 ** 9
+        token = web3.eth.contract(address=web3.toChecksumAddress(
+            config.TTN_CONTRACT), abi=config.TTN_CONTRACT_ABI)
+        balance = token.functions.balanceOf(
+            user_info['address']).call() / 10 ** 9
     elif currency == "busd":
-        token = web3.eth.contract(address=web3.toChecksumAddress(config.BUSD_CONTRACT), abi=config.TTN_CONTRACT_ABI)
-        balance = token.functions.balanceOf(user_info['address']).call() / 10 ** 18
+        token = web3.eth.contract(address=web3.toChecksumAddress(
+            config.BUSD_CONTRACT), abi=config.TTN_CONTRACT_ABI)
+        balance = token.functions.balanceOf(
+            user_info['address']).call() / 10 ** 18
     else:
         bot.answer_callback_query(message.id, "Currency 404", show_alert=True)
         return None
 
-    if balance < new_price_type: 
+    if balance < new_price_type:
+        if currency == "busd":
+            bot.answer_callback_query(message.id, "You Only Have {balance} USDT, But According To Current Subscription Price The Amount Is {sub_amount} USDT".format(
+                balance=round(balance, 4),
+                currency=currency,
+                sub_amount=round(new_price_type, 4)
+            ), show_alert=True)
+            return None
+
         bot.answer_callback_query(message.id, "You Only Have {balance} {currency}, But According To Current Subscription Price The Amount Is {sub_amount} {currency}".format(
             balance=round(balance, 4),
             currency=currency,
@@ -59,25 +77,30 @@ def sameSection(message: types.CallbackQuery, bot: TeleBot, order_id, currency):
         return None
 
     if not 'owner_wallet' in owner_info:
-        bot.answer_callback_query(message.id, "This Group Owner Didn't Set His/Her Address To Receive Payment, Please Contact Them & Ask To Set", show_alert=True)
+        bot.answer_callback_query(
+            message.id, "This Group Owner Didn't Set His/Her Address To Receive Payment, Please Contact Them & Ask To Set", show_alert=True)
         return None
 
-    if not owner_info['owner_wallet']: 
-        bot.answer_callback_query(message.id, "This Group Owner Didn't Set His/Her Address To Receive Payment, Please Contact Them & Ask To Set", show_alert=True)
+    if not owner_info['owner_wallet']:
+        bot.answer_callback_query(
+            message.id, "This Group Owner Didn't Set His/Her Address To Receive Payment, Please Contact Them & Ask To Set", show_alert=True)
         None
 
     return order_info, payment_contract, owner_info, new_price_type, user_info
 
+
 def upgrade(message: types.CallbackQuery, bot: TeleBot):
     command, order_id, currency = message.data.split(" ")
     function_output = sameSection(message, bot, order_id, currency)
-    if not function_output: pass
+    if not function_output:
+        pass
     order_info, payment_contract, owner_info, new_price_type, user_info = function_output
 
-    membership_info = DB['memberships'].find_one({"chat_id": int(order_info['chat_id']), "user_id": message.from_user.id})
+    membership_info = DB['memberships'].find_one(
+        {"chat_id": int(order_info['chat_id']), "user_id": message.from_user.id})
 
-
-    if currency == "ttn": return token_functions.acceptPaymentTTN(
+    if currency == "ttn":
+        return token_functions.acceptPaymentTTN(
             recepient_address=owner_info['owner_wallet'],
             amount_token=new_price_type,
             order_id=order_id,
@@ -103,7 +126,7 @@ def upgrade(message: types.CallbackQuery, bot: TeleBot):
             database_entries=False,
             membership_info=membership_info
         )
-    
+
     buyFunc = payment_contract.functions.acceptPaymentBNB(
         owner_info['owner_wallet'],
         order_id
@@ -119,17 +142,18 @@ def upgrade(message: types.CallbackQuery, bot: TeleBot):
 
     try:
         tx['gas'] = web3.eth.estimate_gas(tx)
-        tx['nonce'] = web3.eth.getTransactionCount(web3.eth.account.privateKeyToAccount(user_info['private_key']).address)
+        tx['nonce'] = web3.eth.getTransactionCount(
+            web3.eth.account.privateKeyToAccount(user_info['private_key']).address)
 
-        signedTx = web3.eth.account.sign_transaction(tx, private_key=user_info['private_key'])
+        signedTx = web3.eth.account.sign_transaction(
+            tx, private_key=user_info['private_key'])
         broadcastedTx = web3.eth.send_raw_transaction(signedTx.rawTransaction)
     except Exception as err:
         return bot.send_message(message.message.chat.id, "<b>Error While Sending TX</b>: <code>{}</code>".format(err), parse_mode='HTML')
 
     result = web3.eth.wait_for_transaction_receipt(broadcastedTx)
-    if result.status == 0: return bot.send_message(message.message.chat.id, "<b>Error After Brodcasting TX</b>: Tx Hash: https://bscscan.com/tx/{}".format(broadcastedTx.hex()))
-
-
+    if result.status == 0:
+        return bot.send_message(message.message.chat.id, "<b>Error After Brodcasting TX</b>: Tx Hash: https://bscscan.com/tx/{}".format(broadcastedTx.hex()))
 
     DB['payment_infos'].insert_one({
         "amount": order_info['price'],
@@ -142,13 +166,14 @@ def upgrade(message: types.CallbackQuery, bot: TeleBot):
             "chat_id": int(order_info['chat_id']),
             "user_id": message.from_user.id
         },
-        {"$set":{
+        {"$set": {
             "is_one_time": True if order_info['payment_type'] == "permanent" else False,
             "end_time": membership_info['end_time'] + timedelta(days=30)}
-        }
+         }
     )
 
-    user_info = bot.get_chat_member(chat_id=order_info['chat_id'], user_id=message.from_user.id)
+    user_info = bot.get_chat_member(
+        chat_id=order_info['chat_id'], user_id=message.from_user.id)
     # print(user_info)
     if user_info.status == "left":
         invite_link = bot.create_chat_invite_link(
@@ -167,18 +192,24 @@ def upgrade(message: types.CallbackQuery, bot: TeleBot):
             success_text
         )
 
-    bot.edit_message_text(message_id=message.message.id, chat_id=message.message.chat.id, text="Membership Upgraded Successfully", reply_markup=None)
+    bot.edit_message_text(message_id=message.message.id, chat_id=message.message.chat.id,
+                          text="Membership Upgraded Successfully", reply_markup=None)
+
 
 def pay(message: types.CallbackQuery, bot: TeleBot):
     command, order_id, currency = message.data.split(" ")
+    print(currency)
     function_output = sameSection(message, bot, order_id, currency)
     # print(function_output)
-    if not function_output: return
+    if not function_output:
+        return
     order_info, payment_contract, owner_info, new_price_type, user_info = function_output
 
-    is_membership = DB['memberships'].find_one({"chat_id": int(order_info['chat_id']), "user_id": message.from_user.id})
+    is_membership = DB['memberships'].find_one(
+        {"chat_id": int(order_info['chat_id']), "user_id": message.from_user.id})
     if is_membership:
-        if is_membership['is_one_time']: return bot.answer_callback_query(message.id, "You already have purchase lifetime susbcription", show_alert=True)
+        if is_membership['is_one_time']:
+            return bot.answer_callback_query(message.id, "You already have purchase lifetime susbcription", show_alert=True)
         return bot.edit_message_text(
             text="<i>You already subscribed to this community, do you want to upgrade?</i>",
             chat_id=message.message.chat.id,
@@ -187,7 +218,8 @@ def pay(message: types.CallbackQuery, bot: TeleBot):
             parse_mode='HTML'
         )
 
-    if currency == "ttn": return token_functions.acceptPaymentTTN(
+    if currency == "ttn":
+        return token_functions.acceptPaymentTTN(
             recepient_address=owner_info['owner_wallet'],
             amount_token=new_price_type,
             order_id=order_id,
@@ -229,19 +261,22 @@ def pay(message: types.CallbackQuery, bot: TeleBot):
 
     try:
         tx['gas'] = web3.eth.estimate_gas(tx)
-        tx['nonce'] = web3.eth.getTransactionCount(web3.eth.account.privateKeyToAccount(user_info['private_key']).address)
+        tx['nonce'] = web3.eth.getTransactionCount(
+            web3.eth.account.privateKeyToAccount(user_info['private_key']).address)
 
-        signedTx = web3.eth.account.sign_transaction(tx, private_key=user_info['private_key'])
+        signedTx = web3.eth.account.sign_transaction(
+            tx, private_key=user_info['private_key'])
         broadcastedTx = web3.eth.send_raw_transaction(signedTx.rawTransaction)
     except Exception as err:
         return bot.send_message(message.message.chat.id, "<b>Error While Sending TX</b>: <code>{}</code>".format(err), parse_mode='HTML')
 
     result = web3.eth.wait_for_transaction_receipt(broadcastedTx)
-    if result.status == 0: return bot.send_message(message.message.chat.id, "<b>Error After Brodcasting TX</b>: Tx Hash: https://bscscan.com/tx/{}".format(broadcastedTx.hex()))
+    if result.status == 0:
+        return bot.send_message(message.message.chat.id, "<b>Error After Brodcasting TX</b>: Tx Hash: https://bscscan.com/tx/{}".format(broadcastedTx.hex()))
 
     DB['groups'].update_one(
         {"_id": int(order_info['chat_id'])},
-        {"$inc": {"total_earn": float(order_info['price']) }}
+        {"$inc": {"total_earn": float(order_info['price'])}}
     )
 
     DB['payment_infos'].insert_one({
@@ -254,7 +289,7 @@ def pay(message: types.CallbackQuery, bot: TeleBot):
         "chat_id": int(order_info['chat_id']),
         "user_id": message.from_user.id,
         "is_one_time": True if order_info['payment_type'] == "permanent" else False,
-        "end_time": datetime.utcnow() + timedelta(days=30),     
+        "end_time": datetime.utcnow() + timedelta(days=30),
         "tx_hash": broadcastedTx.hex(),
         "currency": currency,
         "is_active": True
@@ -275,5 +310,3 @@ def pay(message: types.CallbackQuery, bot: TeleBot):
         message.message.chat.id,
         success_text
     )
-
-    
--- test.py
+++ test.py
@@ -2,8 +2,8 @@ from web3 import Web3
 import config
 
 
-PAYMENT_CONTRACT = "0x3fba9c7f943068064de77394016dc0995ddea228"
-TTN_CONTRACT = Web3.toChecksumAddress("0x5353A64D9231C6c541183c70Ef245295b73EF4FF")
+PAYMENT_CONTRACT = "0xf0d643F0D5AA17e2764A9C1d4CaF3668f7165886"
+TTN_CONTRACT = Web3.toChecksumAddress("0x722b7d4b9c830199F043eA210125fA13a91c64EF")
 
 web3 = Web3(Web3.HTTPProvider("https://rpc.ankr.com/bsc_testnet_chapel")) # Creating Web3 Object
 contract = web3.eth.contract(address=web3.toChecksumAddress(PAYMENT_CONTRACT), abi=config.PAYMENT_CONTRACT_ABI) # Creating Web3 Object
@@ -15,7 +15,7 @@ build_tx = {
     'chainId': chain_id,
 
     'gasPrice': web3.eth.gas_price,
-    'from': web3.toChecksumAddress("0xAad6a88877E6AB7FbC33fdAce672780A85Fc88a8")
+    'from': web3.toChecksumAddress("0xd302f9AA2a57eA2516835A6e36CC168ae0365B37")
 } # Constructing TX Object
 
 try:
@@ -23,7 +23,7 @@ try:
     # token_decimal = token_contract.functions.decimals().call()
 
     allowance = token_contract.functions.allowance(
-        web3.toChecksumAddress("0xAad6a88877E6AB7FbC33fdAce672780A85Fc88a8"),
+        web3.toChecksumAddress("0xd302f9AA2a57eA2516835A6e36CC168ae0365B37"),
         web3.toChecksumAddress(PAYMENT_CONTRACT)
     ).call()
     
@@ -36,12 +36,12 @@ try:
                     'chainId': chain_id,
                     # 'value': int(10 * 10 ** 9),
                     'gasPrice': web3.eth.gas_price,
-                    'from': web3.toChecksumAddress("0xAad6a88877E6AB7FbC33fdAce672780A85Fc88a8")
+                    'from': web3.toChecksumAddress("0xd302f9AA2a57eA2516835A6e36CC168ae0365B37")
                 }
             )
         
             tx['gas'] = web3.eth.estimate_gas(tx)
-            tx['nonce'] = web3.eth.get_transaction_count(web3.toChecksumAddress("0xAad6a88877E6AB7FbC33fdAce672780A85Fc88a8"))
+            tx['nonce'] = web3.eth.get_transaction_count(web3.toChecksumAddress("0xd302f9AA2a57eA2516835A6e36CC168ae0365B37"))
             signed_tx = web3.eth.account.sign_transaction(tx, "775f08f93da3b54168891bab5e52ad737c0d64e4ad015c0e57cba8e6492f5e16")
             tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
             web3.eth.wait_for_transaction_receipt(web3.toHex(tx_hash), timeout=10)
